39cf175e0fde81a2c22bc9048d0e36be97d1eb88
dudassmelo <dudassmelo.2@gmail.com>
2024-01-10 16:57:48 -0300

    first commit

---
+++ src/scripts/engine.js
@@ -0,0 +1,60 @@
+const emojis = [
+    "👀",
+    "👀",
+    "🎁",
+    "🎁",
+    "💖",
+    "💖",
+    "✨",
+    "✨",
+    "😊",
+    "😊",
+    "🐱‍👤",
+    "🐱‍👤",
+    "😎",
+    "😎",
+    "👌",
+    "👌"
+];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(()=> (Math.random()> 0.5 ? 2 : -1));
+
+for(let i=0; i < emojis.length; i++) {
+    let box = document.createElement ("div"); 
+    box.className = "item";
+    box.innerHTML = shuffleEmojis [i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild (box)
+}
+
+function handleClick () {
+    if (openCards.length <2) {
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+    if (openCards.length == 2) {
+        setTimeout (checkMath, 500);
+    }
+
+}
+
+function checkMath () {
+    if (openCards [0].innerHTML === openCards [1].innerHTML) {
+        openCards [0].classList.add
+        ("boxMatch");
+        openCards [1].classList.add
+        ("boxMatch");
+    } else {
+        openCards [0].classList.remove
+        ("boxOpen");
+        openCards [1].classList.remove
+        ("boxOpen");
+    }
+
+    openCards = [];
+
+    if (document.querySelectorAll(".boxMatch").length === emojis.length) {
+        alert("Você venceu!")
+    }
+}
